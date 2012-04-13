778545d4587054e48a5e048aa07c1522797c46a0
Beecher Adams <koalaos127@gmail.com>
2012-04-12 18:12:23 -0700

    Removed link to browser, iTranmission 2.1beta

---
+++ iTransmission/ITBlocklist.h
@@ -0,0 +1,13 @@
+//
+//  ITBlocklist.h
+//  iTransmission
+//
+//  Created by user on 4/10/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ITBlocklist : UIView
+
+@end
---
+++ iTransmission/ITBlocklist.m
@@ -0,0 +1,13 @@
+//
+//  ITBlocklist.m
+//  iTransmission
+//
+//  Created by user on 4/10/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "ITBlocklist.h"
+
+@implementation ITBlocklist
+
+@end
