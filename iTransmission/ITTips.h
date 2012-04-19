d7b9f3f96aceb28a131102323c0511a2fb56aef1
Beecher Adams <koalaos127@gmail.com>
2012-04-18 18:43:15 -0700

    iTransmission 2.2b

---
+++ iTransmission/ITTips.h
@@ -0,0 +1,13 @@
+//
+//  ITTips.h
+//  iTransmission
+//
+//  Created by user on 4/18/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ITTips : UIViewController
+
+@end
---
+++ iTransmission/ITWebinterface.h
@@ -0,0 +1,13 @@
+//
+//  ITWebinterface.h
+//  iTransmission
+//
+//  Created by user on 4/16/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ITWebinterface : UIViewController
+
+@end
