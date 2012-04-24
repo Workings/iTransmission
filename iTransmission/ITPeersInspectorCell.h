f83572c2e89c2ea153368dbe021f1abea6abf13b
Beecher Adams <koalaos127@gmail.com>
2012-04-24 16:41:14 -0700

    More work on Peer inspector

---
+++ iTransmission/ITPeersInspectorCell.h
@@ -0,0 +1,13 @@
+//
+//  ITPeersInspectorCell.h
+//  iTransmission
+//
+//  Created by user on 4/23/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ITPeersInspectorCell : UIViewController
+
+@end
